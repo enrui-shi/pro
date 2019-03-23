530bc875e9fe840dadd91337018be3ceec1aac5c
stone <enrui.shi@stonybrook.edu>
2019-03-23 16:28:35 -0400

    add dotenv

--- main/index.js
+++ main/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+
+//get env
+require('dotenv').config()
 //file dependecy
 
 //routers
@@ -12,11 +15,18 @@ var verify = require('./routers/verify.js');
 var questions = require('./routers/questions.js');
 var search = require('./routers/search.js')
 //api endpoint
+app.use('/login', login);
+app.use('/adduser', adduser);
+app.use('/verify', verify);
+app.use('/logout', logout);
+app.use('/questions',questions);
+app.use('/search',search);
 
 app.get('/',function(req,res){
     res.send('INDEX page');
     //res.sendFile(path.join(__dirname+'/html/index.html'));
 })
 app.listen(port,'0.0.0.0', () => {
+    console.log(process.env.TEST);
     return console.log(`App listening on port ${port}!`);
 })
--- main/routers/adduser.js
+++ main/routers/adduser.js
@@ -2,6 +2,28 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var path = require('path');
+var request = require('request');
+var bodyParser = require('body-parser');
+// create application/json parser
+var jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+router.post('/',jsonParser,function(req,res){
+    data = req.body;
+    var options = {  
+        url: process.env.USER_SERVER,
+        path: '/adduser',
+        method: 'POST',
+        json: data
+    };
+    //send request to USER server
+    request(options, function(err, res, body) {  
+        let json = JSON.parse(body);
+        console.log(json);
+    });
+});
 
 
 
