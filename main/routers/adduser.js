83bafe6bb6918d3726ef4623613258a4c65dcc49
stone <enrui.shi@stonybrook.edu>
2019-03-23 18:24:50 -0400

    fix data

--- main/routers/adduser.js
+++ main/routers/adduser.js
@@ -1,32 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
-var path = require('path');
-var request = require('request');
-var bodyParser = require('body-parser');
+const path = require('path');
+//const nodemailer = require('nodemailer');
+
 // create application/json parser
 var jsonParser = bodyParser.json()
+ 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 router.post('/',jsonParser,function(req,res){
     data = req.body;
-    console.log("data: ", data)
-    var options = {  
-        url: process.env.SERVER_USER+"/adduser",
-        method: 'POST',
-        json: data
-    };
-    //send request to USER server
-    request(options, function(err, response, body) {  
-        if(err){
-            console.log("ERROR")
-            console.log(err);
-        }
-        console.log("req: ",body);
-        res.json(body);
-    });
+    console.log("data: "+data);
+    res.json({"user":"a","email":"aa"})
 });
 
 
