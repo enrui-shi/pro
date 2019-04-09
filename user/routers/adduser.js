78540a176302a577dab6152cb4f5967d1e072c83
stone <enrui.shi@stonybrook.edu>
2019-04-08 20:14:16 -0400

    a

--- user/routers/adduser.js
+++ user/routers/adduser.js
@@ -25,8 +25,8 @@ router.post('/',jsonParser,function(req,res){
             res.json({'status':"error","error":err})
         }else{
             //console.log(data.username+" signed up");
-            console.log(a.ops[0]);
-            sendMail(data);
+            console.log(a.ops[0].username+" signed up");
+            sendMail(a.ops[0]);
             res.json({'status':"OK"});
         }
         
