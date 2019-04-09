8de7b9e96b3342b2a5847401305754aa34eb9b4a
stone <enrui.shi@stonybrook.edu>
2019-04-08 20:20:19 -0400

    a

--- user/routers/verify.js
+++ user/routers/verify.js
@@ -5,22 +5,22 @@ var router = express.Router();
 const path = require('path');
 
 router.post('/',jsonParser,function(req,res){
-    data = req.body;
+    body = req.body;
     json = {'status':'OK'};
-    console.log(data);
+    console.log(body);
     //console.log(data.email);
     //console.log(data['key']);
     var db = req.app.locals.db;
-    db.collection('users').find({ 'email': data['email'] 
+    db.collection('users').find({ 'email': body['email'] 
     }).toArray(function(err, result){
         if(err){
             res.json({'status':'error','error':err});
         }else{
             if(result.length==1){
                 result=result[0];
-                if(result.key==data.key||data.key=='abracadabra'){
+                if(result.key==body.key||data.key=='abracadabra'){
                     console.log("verifed");
-                    db.collection('users').update({'email': data['email']},{ $set:
+                    db.collection('users').update({'email': body['email']},{ $set:
                         {
                         'valide': 'true'
                         }
