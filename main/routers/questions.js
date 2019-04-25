f70306fed679642e756e29d09202b0b30bba3b63
stone <enrui.shi@stonybrook.edu>
2019-04-25 01:26:56 -0400

    change session to expres session

--- main/routers/questions.js
+++ main/routers/questions.js
@@ -9,8 +9,8 @@ var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.post('/add',jsonParser,function(req,res){
-    req.body.current_user = req.cookies;
-    console.log("current session:", req.cookies.current_user)
+    req.body.current_user = req.cookies.session.current_user;
+    console.log("current session:", req.cookies.session)
     var forward_url = process.env.SERVER_QUESTION+"/questions/add"
     console.log('request send to ',forward_url);
     if(process.env.DEBUG&&false){
