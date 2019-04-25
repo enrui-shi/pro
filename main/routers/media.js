88bd320143db8a62ee4bd87fa2ee4cad524d8a35
stone <enrui.shi@stonybrook.edu>
2019-04-25 02:16:35 -0400

    change session to expres session

--- main/routers/media.js
+++ main/routers/media.js
@@ -10,7 +10,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 router.get('/:id',jsonParser,function(req,res){
-    req.body.current_user = req.session.current_user;
+    //req.body.current_user = req.current_user;
     var forward_url = process.env.SERVER_MDEIA+"/meida"+req.params.id;
     console.log('request send to ',forward_url);
     if(process.env.DEBUG){
