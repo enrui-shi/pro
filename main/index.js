b700aaf3a2255d6266b5d23bfb108fa24040588f
stone <enrui.shi@stonybrook.edu>
2019-04-24 22:10:02 -0400

    change session to expres session

--- main/index.js
+++ main/index.js
@@ -23,7 +23,6 @@ app.use(session({
     resave: false,   
     saveUninitialized: true,
     cookie: {  
-        domain: "356.stone",  
         maxAge: 1000*30*60  
     },  
     secret: "lalala"}));
