a2920665fa0f65c9a6208e72d3b127edb6f45137
stone <enrui.shi@stonybrook.edu>
2019-05-13 20:44:38 -0400

    chage

--- main/script/main.js
+++ main/script/main.js
@@ -1,10 +1,15 @@
 
-$(document).ready(function () {
+
+
+
+
+
+
+function showQuestion(json){
     var ul = $('<ul>').appendTo('body');
-    var json = { items: ['item 1', 'item 2', 'item 3'] };
     $(json.items).each(function(index, item) {
         ul.append(
             $(document.createElement('li')).text(item)
         );
     })
-});
+}
